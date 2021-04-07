5dad836f828d6ba4d5302e13518ab0efce8a5070
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-04-07 12:48:24 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -51,12 +51,6 @@ bool SendToConsoleAlert::sendAlert(BreachType breachType)
     return true;
 }
 
-
-void sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
-
 map<BreachType, char*> BreachMessage = {
   {NORMAL, "normal"},
   {TOO_LOW, "too Low"},
